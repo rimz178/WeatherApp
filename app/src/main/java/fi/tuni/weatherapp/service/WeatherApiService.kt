58ec8969c9920cee9f946109ef307842a8424666
Riku <riku.mikkonen@tuni.fi>
2022-09-15 15:35:00 +0300

    hide api key 2

--- app/src/main/java/fi/tuni/weatherapp/service/WeatherApiService.kt
+++ app/src/main/java/fi/tuni/weatherapp/service/WeatherApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class weatherApiService {
-    //https://api.openweathermap.org/data/2.5/weather?q=tampere&appid=9853ec5ecf367cb385919fb2caf40185
+  
 
 
     private val  BASE_URL ="https://api.openweathermap.org/"
