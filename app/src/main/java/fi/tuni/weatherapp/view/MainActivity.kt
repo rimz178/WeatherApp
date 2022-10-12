4ea25006cf690b55d68032f2598f8c1cd729b3e9
Riku <riku.mikkonen@tuni.fi>
2022-10-12 16:57:40 +0300

    the location works
    Asks the user for the location when the user first opens the app and displays weather information for the location

--- app/src/main/java/fi/tuni/weatherapp/view/MainActivity.kt
+++ app/src/main/java/fi/tuni/weatherapp/view/MainActivity.kt
@@ -1,32 +1,44 @@
 package fi.tuni.weatherapp.view
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
 import android.icu.text.DecimalFormat
 import android.icu.text.SimpleDateFormat
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import fi.tuni.weatherapp.R
 import fi.tuni.weatherapp.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import java.time.Instant
-import java.time.ZoneId
 import java.util.*
+import android.text.Editable as Editable1
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    private  lateinit var  fused : FusedLocationProviderClient
+
     private lateinit var viewmodel: MainViewModel
     private lateinit var GET: SharedPreferences
     private lateinit var SET: SharedPreferences.Editor
- 
-
     private val df = DecimalFormat("#")
 
 
@@ -39,38 +51,23 @@ class MainActivity : AppCompatActivity() {
         SET = GET.edit()
         viewmodel = ViewModelProviders.of(this)[MainViewModel::class.java]
 
-
-        var cName = GET.getString("cityName", "Tampere")?.toLowerCase()
+        var cName = GET.getString("cityName","")?.toLowerCase()
         edt_city_name.setText(cName)
         viewmodel.refreshData(cName!!)
 
-        /*   val locationPermissionRequest = registerForActivityResult(
-           ActivityResultContracts.RequestMultiplePermissions()
-       ) { permissions ->
-           when {
-               permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                   Toast.makeText(applicationContext,"Sijainti päällä", Toast.LENGTH_LONG).show()
-               }
-               permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-
-               } else -> {
-               Toast.makeText(applicationContext,"Sijainti ei ole päällä", Toast.LENGTH_LONG).show()
-           }
-           }
+        fused = LocationServices.getFusedLocationProviderClient(this)
+2
 
 
-       locationPermissionRequest.launch(arrayOf(
-           Manifest.permission.ACCESS_FINE_LOCATION,
-           Manifest.permission.ACCESS_COARSE_LOCATION))
-}*/
         getLiveData()
+        getCurrentLocation()
 
         swipe_refresh_layout.setOnRefreshListener {
             mainContent.visibility = View.GONE
             tv_error.visibility = View.GONE
             pb_loading.visibility = View.GONE
 
-            var cityName = GET.getString("cityName", cName)?.toLowerCase()
+            val cityName = GET.getString("cityName", cName)?.toLowerCase()
             edt_city_name.setText(cityName)
             viewmodel.refreshData(cityName!!)
             swipe_refresh_layout.isRefreshing = false
@@ -80,7 +77,7 @@ class MainActivity : AppCompatActivity() {
             val cityName = edt_city_name.text.toString()
             SET.putString("cityName", cityName)
             SET.apply()
-            viewmodel.refreshData(cityName!!)
+            viewmodel.refreshData(cityName)
             getLiveData()
 
         }
@@ -88,11 +85,134 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @SuppressLint("MissingPermission","SetTextI18n")
+    private  fun getCurrentLocation() {
+
+        if(checkPermission())
+        {
+            if(ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            )!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            )!= PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions()
+                return
+            }
+
+            if(isLocationEnabled())
+            {
+                    fused.lastLocation.addOnCompleteListener(this) {task ->
+                        val location: Location? =task.result
+
+                        if(location==null) {
+
+                            Toast.makeText(this, "null Received", Toast.LENGTH_SHORT).show()
+
+                        }
+                        else
+                        {
+                            Toast.makeText(this, "Get success", Toast.LENGTH_SHORT).show()
+                            city.text =  getCityName(location.latitude,location.longitude)
+                            country.text =  getCountryName(location.latitude,location.longitude)
+
+
+                        }
+                    }
+            }
+            else
+            {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else
+        {
+            requestPermissions()
+        }
+    }
+
+    private fun isLocationEnabled() :Boolean{
+        val locationManager: LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION,
+
+        )
+    }
+
+
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 150
+
+    }
+    private fun checkPermission() : Boolean {
+
+        if(ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_COARSE_LOCATION)
+            ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+           Manifest.permission.ACCESS_FINE_LOCATION )== PackageManager.PERMISSION_GRANTED)
+        {
+            return  true
+        }
+        return  false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array< String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode== PERMISSION_REQUEST_ACCESS_LOCATION)
+        {
+            if(grantResults.isNotEmpty()&& grantResults[0]== PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(applicationContext,"granted", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+
+
+            }
+            else
+            {
+                Toast.makeText(applicationContext,"denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun getCityName(lat: Double, long: Double): String {
+        var cityname = ""
+        var geoCoder = Geocoder(this, Locale.getDefault())
+        var adress = geoCoder.getFromLocation(lat,long,3)
+
+        cityname = adress[0].locality
+       Log.d("Debug:", "Your City: $cityname")
+        return cityname
+    }
+    private fun getCountryName(lat: Double,long: Double):String{
+         var countryName = ""
+        var geoCoder = Geocoder(this, Locale.getDefault())
+        var adress = geoCoder.getFromLocation(lat,long,3)
+        countryName = adress[0].countryName
+        return countryName
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("SetTextI18n")
     private fun getLiveData() {
 
-        viewmodel.weather_data.observe(this, Observer { data ->
+        viewmodel.weather_data.observe(this) { data ->
             data?.let {
                 mainContent.visibility = View.VISIBLE
                 pb_loading.visibility = View.GONE
@@ -104,8 +224,7 @@ class MainActivity : AppCompatActivity() {
                 info_weather.text = data.weather[0].description
                 sunrise.text = getTime(data.sys.sunrise.toLong())
                 sunset.text = getTime(data.sys.sunset.toLong())
-         /*    sunset.text = timeStampToLocalDate(data.sys.sunset.toLong())
-                sunrise.text = timeStampToLocalDate(data.sys.sunrise.toLong())*/
+
                 feels_like.text = df.format(data.main.feelsLike).toString() + " °C"
                 humidity.text = data.main.humidity.toString() + "%"
 
@@ -119,8 +238,8 @@ class MainActivity : AppCompatActivity() {
                     .into(img_weather_icon)
 
             }
-        })
-        viewmodel.weather_error.observe(this, Observer { error ->
+        }
+        viewmodel.weather_error.observe(this) { error ->
             error?.let {
                 if (it) {
                     tv_error.visibility = View.VISIBLE
@@ -131,8 +250,8 @@ class MainActivity : AppCompatActivity() {
 
                 }
             }
-        })
-        viewmodel.weather_load.observe(this, Observer { loading ->
+        }
+        viewmodel.weather_load.observe(this) { loading ->
             loading?.let {
                 if (it) {
                     pb_loading.visibility = View.VISIBLE
@@ -142,7 +261,7 @@ class MainActivity : AppCompatActivity() {
                     pb_loading.visibility = View.GONE
                 }
             }
-        })
+        }
 
     }
 
@@ -152,4 +271,7 @@ class MainActivity : AppCompatActivity() {
         return times.format(date)
     }
 
+
+
 }
+
